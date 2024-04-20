ec1104006ad171a52b5cb021da9d29c92e45454b
Christian Garbin <garbin.christian@gmail.com>
2024-04-20 16:21:40 -0400

    Reorganize UI to be more compact

--- app.py
+++ app.py
@@ -14,33 +14,33 @@ st.subheader("A prototype for RAG with all pieces running locally")
 st.image("pics/solution-part2-similarity search-no letters.drawio.png")
 
 
-def show_files_in_store():
+def show_files_in_store(col):
     """Show which files are already in the store."""
-    st.subheader("Files already in the store")
+    col.subheader("Files in the store")
     files_in_store = vector_store.files_in_store()
     if len(files_in_store) == 0:
-        st.write("No documents in the store")
+        col.write("No documents in the store")
     else:
-        with st.expander("Click to see documents (files) already in the store"):
+        with col.expander("Click to see documents (files) already in the store"):
             files_in_store = sorted(files_in_store)
             for file_name in files_in_store:
                 st.write(file_name)
 
 
-def add_file_to_store():
+def add_file_to_store(col):
     """Upload a file to the store."""
-    st.subheader("Add documents (files) to the store")
-    file_to_upload = st.file_uploader("Upload a file (if it's not in the store yet)")
+    col.subheader("Add files")
+    file_to_upload = col.file_uploader("", label_visibility="collapsed")
     if file_to_upload is None:
         return
 
     if vector_store.file_stored(file_to_upload.name):
-        st.write(f"'{file_to_upload.name}' is already in the store")
+        col.write(f"'{file_to_upload.name}' is already in the store")
         return
 
-    with st.spinner("Adding document to the store (please be patient - everything is running on your computer)..."):
+    with col.spinner("Adding document to the store (please be patient - everything is running on your computer)..."):
         # Read the file and save to a local temporary directory (ingestion requires a local file)
-        file_contents = file_to_upload.read()
+        file_contents = file_to_upload.read("")
         with tempfile.TemporaryDirectory() as temp_dir:
             with open(f"{temp_dir}/{file_to_upload.name}", "wb") as temp_file:
                 temp_file.write(file_contents)
@@ -51,7 +51,7 @@ def answer_question():
     """Let the user ask a question and retrieve the answer."""
     st.subheader("Ask a question")
     question = st.text_input(
-        "Question", placeholder="Ask a question  - press Enter to submit", label_visibility="hidden"
+        "Question", placeholder="Ask a question  - press Enter to submit", label_visibility="collapsed"
     )
     if question is None or question == "":
         return
@@ -69,6 +69,8 @@ def answer_question():
                     st.write(chunk)
 
 
-show_files_in_store()
-add_file_to_store()
+col_add_file, col_files_in_store = st.columns(2)
+
+add_file_to_store(col_add_file)
+show_files_in_store(col_files_in_store)
 answer_question()
