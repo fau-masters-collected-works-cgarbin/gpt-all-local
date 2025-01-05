829cf983064f547baada467120abb36d5da47bb1
Christian Garbin <garbin.christian@gmail.com>
2025-01-05 09:24:48 -0500

    Adapt to new package versions and improve progress reporting

--- vector_store.py
+++ vector_store.py
@@ -1,11 +1,13 @@
 """Vector store functions to abstract it from the rest of the code."""
+
 import chromadb
+import langchain_chroma
+import langchain_huggingface
 from chromadb.config import Settings
 from langchain.docstore.document import Document
-from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain_community.vectorstores import Chroma
 
 import constants
+import logger
 
 CHROMA_SETTINGS = Settings(
     # Configure Chroma for persistence
@@ -14,10 +16,33 @@ CHROMA_SETTINGS = Settings(
     anonymized_telemetry=False,
 )
 
-_embeddings = HuggingFaceEmbeddings(model_name=constants.EMBEDDINGS_MODEL_NAME)
+_DB = None
 
-_client = chromadb.PersistentClient(settings=CHROMA_SETTINGS, path=constants.STORAGE_DIR)
-_db = Chroma(persist_directory=constants.STORAGE_DIR, embedding_function=_embeddings, client=_client)
+
+def _prepared() -> bool:
+    """Check if the store is prepared."""
+    return _DB is not None
+
+
+def _prepare():
+    """Prepare the vector store.
+
+    Notes:
+      - *Not* thread-safe.
+    """
+    if _prepared():
+        return
+
+    global _DB  # pylint: disable=global-statement
+
+    log = logger.get_logger()
+    log.debug("Preparing the vector store")
+    log.debug(f"   Loading the embedding model '{constants.EMBEDDINGS_MODEL_NAME}'")
+    embeddings = langchain_huggingface.HuggingFaceEmbeddings(model_name=constants.EMBEDDINGS_MODEL_NAME)
+
+    log.debug(f"   Creating the vector store in '{constants.STORAGE_DIR}'")
+    client = chromadb.PersistentClient(settings=CHROMA_SETTINGS, path=constants.STORAGE_DIR)
+    _DB = langchain_chroma.Chroma(persist_directory=constants.STORAGE_DIR, embedding_function=embeddings, client=client)
 
 
 def store():
@@ -25,17 +50,14 @@ def store():
 
     Use with caution to not break the abstraction.
     """
-    return _db
+    _prepare()
+    return _DB
 
 
 def add_documents(documents: list[Document]) -> None:
     """Add the given documents to the vector store."""
-    _db.add_documents(documents)
-
-
-def persist():
-    """Persist the vector store."""
-    _db.persist()
+    _prepare()
+    _DB.add_documents(documents)
 
 
 def files_in_store() -> list[str]:
@@ -43,9 +65,11 @@ def files_in_store() -> list[str]:
 
     Only files names are returned, not the full path.
     """
+    _prepare()
+
     # We need to be careful here because we are making assumptions about the format of external data
     try:
-        metadata = _db.get()["metadatas"]
+        metadata = _DB.get()["metadatas"]
         # Remove duplicates
         set_of_files = set()
         for file_name in metadata:
@@ -62,6 +86,8 @@ def file_stored(file_name: str) -> bool:
       - It doesn't check if the file is in the store with a different name.
       - It doesn't check if the file contents are the same.
     """
+    _prepare()
+
     # We compare only the file name, not the full path
     file_name = file_name.split("/")[-1]
     files_already_in_store = files_in_store()
